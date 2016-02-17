e29a4a5cde9cbc7b009a550b4ec22045649f3de6
rmk <malyshkov_roman@mail.ru>
2016-02-17 22:12:17 +0600

    Added mainjavacode.java Fixed some stuff in mainjavacode

--- mainjavacode.java
+++ mainjavacode.java
@@ -1,11 +1,13 @@
-import java.swing.*;
+import javax.swing.*;
 
 public class mainjavacode{
+
     JFrame frame = new JFrame("EleGiggle");
-    JPanel panel = new JPanel;
-    JButton myButton = new Button("OK");
-    panel.add(myButton);
-    frame.addcontentpane().add(panel);
+    JPanel panel = new JPanel();
+    JButton button = new JButton("OK");
+
+    panel.add(button);
+    frame.getContentPane().add(panel);
     frame.show();
 
 }
